4cf3768a198756ba24ac6940a97812b587eff61a
vanemi02 <misavanek20@seznam.cz>
2023-11-30 14:51:37 +0100

    locationSearchBar

--- src/components/LocationSearchBar.tsx
+++ src/components/LocationSearchBar.tsx
@@ -1,28 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { MouseEventHandler, useEffect, useRef, useState } from 'react'
 import SearchBar from './SearchBar'
 import citiesImport from "../../city.list.json"
-type Props = {}
+import type { coord, City } from "../Types"
 
-type City = {
-  id: number,
-  name: string,
-  state: string,
-  country: string,
-  coord: {
-    lon: number,
-    lat: number
-  }
+type Props = {
+  setCoord: (coord: coord) => void
 }
 
-const cityNames = (citiesImport as City[]).map(city => city.name)
+const citySuggestions = (citiesImport as City[]).map(city => {
+  return {
+    suggestion: city.name,
+    city
+  }
+})
 
-export default function LocationSearchBar({}: Props) {
+export default function LocationSearchBar({ setCoord }: Props) {
+  const searchBarValue = useRef<City | string>()
 
-  useEffect(() => {
-    const cities: City[] = citiesImport as City[]
-  })
-  
-  return (
-    <SearchBar suggestions={cityNames}/>
-  )
+  function handleSearchButton(): void {
+    if (!searchBarValue.current) return
+    if (typeof searchBarValue.current === "string") {
+      const foundCity = (citiesImport as City[]).find(city => city.name === searchBarValue.current)
+      if (!foundCity) return
+      setCoord(foundCity.coord)
+    } else {
+      setCoord(searchBarValue.current.coord)
+    }
+  }
+
+  function handleSearchValue(value: typeof citySuggestions[number] | string) { // TODO prepsat na value a ne setValue
+    if (typeof value === "string") {
+      searchBarValue.current = value
+    } else {
+      searchBarValue.current = value.city
+    }
+  }
+
+  function handleGPSButton(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCoord({ lat: position.coords.latitude, lon: position.coords.longitude })
+    })
+  }
+
+  return (<>
+    <SearchBar placeholder='City' setValue={handleSearchValue} suggestions={citySuggestions} />
+    <button onClick={handleSearchButton}>search</button>
+    {navigator.geolocation &&
+      <button onClick={handleGPSButton}>GPS</button>
+    }
+  </>)
 }
