269627093e1a7796f824a137d6fc16a7c092e313
vanemi02 <misavanek20@seznam.cz>
2023-11-30 14:57:17 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import LocationSearchBar from './components/LocationSearchBar'
+import type { weatherApiResponse, coord, weatherData } from './Types'
+import DayOfWeatherCard from './components/DayOfWeatherCard/DayOfWeatherCard'
+
+//TODO README
 
 function App() {
+  const [coord, setCoord] = useState<coord>()
+  const weatherApiResponse = useRef<weatherApiResponse>()
+  const [DaysOfWeatherMap, setDaysOfWeatherMap] = useState<Map<Date, weatherData>[]>()
+  const APIsecret = "a0aac2b9d48313e5f75ccde2316be732" // TODO predelat
+
+  useEffect(() => {
+    if (!coord) return
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${coord.lat}&lon=${coord.lon}&appid=${APIsecret}`)
+      .then(res => res.json())
+      .then(jsonRes => {
+        weatherApiResponse.current = jsonRes
+        const newDaysOfWeatherMap: Map<Date, weatherData>[] = []
+        let weatherMap = new Map()
+        let previousDate = new Date(weatherApiResponse.current!.list[0].dt*1000).getDate()
+        for (const weatherDataPoint of weatherApiResponse.current!.list) {
+          const {temp, humidity, pressure} =  weatherDataPoint.main
+          if (new Date(weatherDataPoint.dt*1000).getDate() !== previousDate) {
+            previousDate = new Date(weatherDataPoint.dt*1000).getDate()
+            newDaysOfWeatherMap.push(weatherMap)
+            weatherMap = new Map()
+          }
+          weatherMap.set(new Date(weatherDataPoint.dt*1000), {humidity: {value: humidity, unit: "%"}, pressure: {value: pressure, unit: "kPa"}, temp: {value: temp, unit: "C"}})
+        }
+        setDaysOfWeatherMap(newDaysOfWeatherMap)
+      })
+  }, [coord])
+
   return (
     <>
-      <LocationSearchBar />
+      <LocationSearchBar setCoord={setCoord} />
+      {DaysOfWeatherMap?.length &&
+        DaysOfWeatherMap.map(weatherMap => <DayOfWeatherCard weatherMap={weatherMap} />) 
+      }
     </>
   )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,26 +1,30 @@
 import React, { useState } from 'react'
-import SearchSuggestion from './SearchSuggestion'
+import SearchSuggestion from './SearchSuggestion/SearchSuggestion'
 
-type Props = {
-  suggestions?: string[]
+type Props<T> = {
+  suggestions?: T[]
+  setValue: (value: string | T) => void
+  placeholder?: string
 }
 
-export default function SearchBar({suggestions}: Props) {
+export default function SearchBar<T extends {suggestion: string}>({suggestions, setValue, placeholder}: Props<T>) {
   const [searchBarValue, setSearchBarValue] = useState("")
   const [isSuggestionHidden, setIsSuggestionHidden] = useState(false)
   
-  function handleSelection(selected: string) {
+  function handleSelection(selected: T) {
     setIsSuggestionHidden(true)
-    setSearchBarValue(selected)
+    setSearchBarValue(selected.suggestion)
+    setValue(selected)
   }
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     setSearchBarValue(e.target.value)
+    setValue(e.target.value)
     setIsSuggestionHidden(false)
   }
   
   return (<>
-    <input onChange={handleInputChange} value={searchBarValue} type="text" />
+    <input placeholder={placeholder} onChange={handleInputChange} value={searchBarValue} type="text" />
     {suggestions && !isSuggestionHidden &&
       <SearchSuggestion onSelection={handleSelection} search={searchBarValue} from={suggestions} />
     }
