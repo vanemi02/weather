66f9a4f832a12cb47bbb6b2efe949d811a5d950c
vanemi02 <misavanek20@seznam.cz>
2023-12-01 22:06:10 +0100

    glow up

--- src/components/DayOfWeatherCard/DayOfWeatherCard.tsx
+++ src/components/DayOfWeatherCard/DayOfWeatherCard.tsx
@@ -1,38 +1,106 @@
-import React, { useState } from 'react'
+import React, { CSSProperties, useState } from 'react'
 import type { weatherData } from "../../Types"
 import css from "./DayOfWeatherCard.module.css"
 import WeatherDataPoint from '../WeatherDataPoint/WeatherDataPoint'
 
 type Props = {
   weatherMap: Map<Date, weatherData>
+  index?: number
 }
 
-export default function DayOfWeatherCard({ weatherMap }: Props) {
+function minMax(number: number, min: number, max: number) {
+  if (number > max) {
+    return max
+  } else if (number < min) {
+    return min
+  }
+  return number
+}
+
+function mapNumber(number: number, fromMin: number, fromMax: number, toMin: number, toMax: number) {
+  const range = Math.abs(fromMax - fromMin)
+  const newNumber = minMax(number, fromMin, fromMax)
+  return (((newNumber - fromMin) / range) * (toMax - toMin)) + toMin
+}
+
+export default function DayOfWeatherCard({ weatherMap, index }: Props) {
+  const stagger = (index ?? 0) * 100
   const [weatherDisplay, setWeatherDisplay] = useState<keyof weatherData>("temp")
-  const weatherDataPoints = []
-  const firstDataPointDate = weatherMap.keys().next().value as Date
+  const weatherDataPoints = [...weatherMap.entries()] as [Date, weatherData][]
+  const weatherDataPointsComponents = []
+  const firstDataPointDate = weatherDataPoints[0][0]
+  const firstDataPoint = weatherDataPoints[0][1]
+  const lastDataPoint = weatherDataPoints[weatherDataPoints.length - 1][1]
   const dayName = firstDataPointDate.toLocaleDateString(undefined, { weekday: "long" })
   const dayAndMonth = firstDataPointDate.toLocaleDateString(undefined, { day: '2-digit', month: '2-digit' })
 
   for (const [date, weatherData] of weatherMap) {
-    weatherDataPoints.push(<WeatherDataPoint display={weatherDisplay} weatherDataPoint={{ date, ...weatherData }} />)
+    weatherDataPointsComponents.push(<WeatherDataPoint key={Math.random()} display={weatherDisplay} weatherDataPoint={{ date, ...weatherData }} />)
+  }
+
+  function genGradient(): CSSProperties {
+    const min = -5
+    const max = 30
+    const warm = [242, 223, 14]
+    const cold = [14, 135, 242]
+    const alpha = "var(--backround-opacity)"
+    const gradientFrom = `rgba(${
+      mapNumber(firstDataPoint.temp.value, min, max, cold[0], warm[0])
+    },
+    ${
+      mapNumber(firstDataPoint.temp.value, min, max, cold[1], warm[1])
+    },
+    ${
+      mapNumber(firstDataPoint.temp.value, min, max, cold[2], warm[2])
+    }, ${alpha})`
+    const gradientTo = `rgba(${
+      mapNumber(lastDataPoint.temp.value, min, max, cold[0], warm[0])
+    },
+    ${
+      mapNumber(lastDataPoint.temp.value, min, max, cold[1], warm[1])
+    },
+    ${
+      mapNumber(lastDataPoint.temp.value, min, max, cold[2], warm[2])
+    }, ${alpha})`
+    
+    return {
+      background: `linear-gradient(90deg, ${gradientFrom}, ${gradientTo})`
+    }
   }
 
   return (<>
-    <div className={css.card}>
+    <div style={{...genGradient(), animationDelay: stagger + "ms"}} className={css.card}>
       <div className={css.cardTop}>
         <div>
           <div className={css.day}>{dayName}</div>
           <div className={css.date}>{dayAndMonth}</div>
         </div>
         <div>
-          <button onClick={() => setWeatherDisplay("temp")}>T</button>
-          <button onClick={() => setWeatherDisplay("pressure")}>P</button>
-          <button onClick={() => setWeatherDisplay("humidity")}>H</button>
+          <button
+            className={weatherDisplay === "temp" ? css.activeButton : ""}
+            onClick={() => setWeatherDisplay("temp")}
+            title="temperature"
+          >
+            <i className="fa-solid fa-temperature-three-quarters"></i>
+          </button>
+          <button
+            className={weatherDisplay === "pressure" ? css.activeButton : ""}
+            onClick={() => setWeatherDisplay("pressure")}
+            title="pressure"
+          >
+            <i className="fa-solid fa-gauge-high"></i>
+          </button>
+          <button
+            className={weatherDisplay === "humidity" ? css.activeButton : ""}
+            onClick={() => setWeatherDisplay("humidity")}
+            title="humidity"
+          >
+            <i className="fa-solid fa-droplet"></i>
+          </button>
         </div>
       </div>
       <div className={css.datapointContainer}>
-        {weatherDataPoints}
+        {weatherDataPointsComponents}
       </div>
     </div>
   </>)
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,32 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, FocusEvent, useRef } from 'react'
 import SearchSuggestion from './SearchSuggestion/SearchSuggestion'
 
 type Props<T> = {
   suggestions?: T[]
-  setValue: (value: string | T) => void
+  onValueChange: (value: string | T) => void
   placeholder?: string
+  inputProps?: React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+  onSelection?: (selected: T) => void
 }
 
-export default function SearchBar<T extends {suggestion: string}>({suggestions, setValue, placeholder}: Props<T>) {
+export default function SearchBar<T extends {suggestion: string}>({suggestions, onValueChange, placeholder, inputProps, onSelection}: Props<T>) {
   const [searchBarValue, setSearchBarValue] = useState("")
   const [isSuggestionHidden, setIsSuggestionHidden] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null)
   
   function handleSelection(selected: T) {
     setIsSuggestionHidden(true)
     setSearchBarValue(selected.suggestion)
-    setValue(selected)
+    onValueChange(selected)
+    inputRef.current?.focus()
+    
+    if (!onSelection) return
+    onSelection(selected)
   }
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearchBarValue(e.target.value)
-    setValue(e.target.value)
     setIsSuggestionHidden(false)
+    setSearchBarValue(e.target.value)
+    onValueChange(e.target.value)
   }
-  
-  return (<>
-    <input placeholder={placeholder} onChange={handleInputChange} value={searchBarValue} type="text" />
+
+  function handleOnBlur(e: FocusEvent<HTMLDivElement, Element>): void {
+    e.stopPropagation()
+    if (e.relatedTarget) return
+    setIsSuggestionHidden(true)
+  }
+
+  return (<div onBlur={handleOnBlur}>
+    <input ref={inputRef} {...inputProps} placeholder={placeholder} onChange={handleInputChange} value={searchBarValue} type="search" />
     {suggestions && !isSuggestionHidden &&
       <SearchSuggestion onSelection={handleSelection} search={searchBarValue} from={suggestions} />
     }
-  </>)
+  </div>)
 }
